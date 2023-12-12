41f5f47690ae8e379ceb9b37b71ed5e3f2c09835
Christine Yoo <christine.y@arketa.co>
2023-12-11 21:24:09 -0800

    data fetching from server component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const UsersPage = () => {
-  return <div>UsersPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UsersPage;
