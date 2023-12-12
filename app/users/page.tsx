685aacd5bfa962207f3af3e0191555c520ceaec2
Christine Yoo <christine.y@arketa.co>
2023-12-11 22:11:10 -0800

    added global css and css module

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,7 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    cache: "no-store",
+    // cache: "no-store",
   });
   const users: User[] = await res.json();
 
