afcee9db9cecb35c44f93ff84143254c7e8e55eb
Christine Yoo <christine.y@arketa.co>
2023-12-26 21:40:14 -0800

    added nested routes

---
+++ app/users/[id]/photos/[photoId]/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props {
+  params: { id: number; photoId: number };
+}
+const PhotoPage = ({ params: { id, photoId } }: Props) => {
+  return (
+    <div>
+      PhotoPage {id} {photoId}
+    </div>
+  );
+};
+
+export default PhotoPage;
