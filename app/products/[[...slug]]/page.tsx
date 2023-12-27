c29ce460577843e75aa054d3b4cdc84b59c5e9b0
Christine Yoo <christine.y@arketa.co>
2023-12-26 21:59:28 -0800

    sort attempt

--- app/products/[[...slug]]/page.tsx
+++ app/products/[[...slug]]/page.tsx
@@ -2,10 +2,14 @@ import React from "react";
 
 interface Props {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 }
 
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>ProductPage {slug}</div>;
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return <div>ProductPage {sortOrder}</div>;
 };
 
 export default ProductPage;
--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { sort } from "fast-sort";
 
 interface User {
   id: number;
@@ -6,21 +7,39 @@ interface User {
   email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+  searchParams: { sortOrder: string };
+}
+
+const UserTable = async ({ searchParams: { sortOrder } }: Props) => {
+  const [ascending, setAscending] = React.useState(true);
+  const [sortedUsers, setSortedUsers] = React.useState<User[]>([]);
+
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     // cache: "no-store",
   });
   const users: User[] = await res.json();
+
+  useEffect(() => {
+    let sorted = [];
+    if (ascending) {
+      sorted = sort(users).asc((user) => user.name);
+    } else {
+      sorted = sort(users).desc((user) => user.name);
+    }
+    setSortedUsers(sorted);
+  }, [ascending]);
+
   return (
     <table className="table table-bordered">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Email</th>
+          <th onClick={() => setAscending(!ascending)}>Name</th>
+          <th onClick={() => setAscending(!ascending)}>Email</th>
         </tr>
       </thead>
       <tbody>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.email}</td>
