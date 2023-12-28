2b688f778d094d13269e59062c5afb7056067131
Christine Yoo <christine.y@arketa.co>
2023-12-28 09:57:23 -0800

    added NavBar

---
+++ app/NavBar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="flex bg-slate-200 p-5">
+      <Link href="/" className="mr-5">
+        Next.js
+      </Link>
+      <Link href="/users" className="mr-5">
+        Users
+      </Link>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AdminHomePage = () => {
+  return <div>Admin HomePage</div>;
+};
+
+export default AdminHomePage;
