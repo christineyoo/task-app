552575577ef538557f42de23141e6681cb6696c2
Christine Yoo <christine.y@arketa.co>
2024-01-20 14:36:48 -0800

    Implemented upload component

---
+++ app/upload/page.tsx
@@ -0,0 +1,42 @@
+"use client";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+
+const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="A coffee image"
+        />
+      )}
+
+      <CldUploadWidget
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+        uploadPreset="rkm3oaer"
+      >
+        {({ open }) => {
+          return (
+            <button className="btn btn-primary" onClick={() => open()}>
+              Upload an Image
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UploadPage;
