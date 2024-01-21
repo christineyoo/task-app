4d257444b4e9d8bb0088cd017d5aa740db046149
Christine Yoo <christine.y@arketa.co>
2024-01-20 17:57:07 -0800

    added auth and middleware

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+"use client";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const { status, data: session } = useSession();
+
   return (
     <div className="flex bg-slate-200 p-5">
       <Link href="/" className="mr-5">
@@ -10,6 +14,19 @@ const NavBar = () => {
       <Link href="/users" className="mr-5">
         Users
       </Link>
+      {status === "authenticated" && (
+        <div>
+          {session.user!.name}
+          <Link href="api/auth/signout" className="ml-3">
+            Sign out
+          </Link>
+        </div>
+      )}
+      {status === "unauthenticated" && (
+        <Link href="/api/auth/signin" className="mr-5">
+          Login
+        </Link>
+      )}
     </div>
   );
 };
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,16 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-// const handler = NextAuth({});
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
 
-// export { handler as GET, handler as POST };
+// Set the providers
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
